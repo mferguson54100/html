8074200a96bd668e5f163857fddaf7f265061562
michael Ferguson <mferguson54100@gmail.com>
2018-06-20 10:33:48 -0700

    Initial commit

---
+++ New folder/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.BackColor = System.Drawing.SystemColors.Highlight
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMessage.Font = New System.Drawing.Font("Segoe UI Black", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(90, 79)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(91, 15)
+        Me.lblMessage.TabIndex = 0
+        Me.lblMessage.Text = "Clic en el boton"
+        Me.lblMessage.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(72, 174)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(122, 23)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "GoogleTranslate"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents btn1 As Button
+End Class
---
+++ New folder/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        lblMessage.Text = "Hola Mundo"
+
+    End Sub
+
+    Private Sub lblMessage_Click(sender As Object, e As EventArgs) Handles lblMessage.Click
+
+    End Sub
+End Class
